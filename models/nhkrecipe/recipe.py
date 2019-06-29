e26057a2661f705207c2563b4fe54aa3d599abb5
K26856 <0delta0alpha@gmail.com>
2019-06-29 15:30:49 +0900

    add get_random_recipe_by_params

--- models/nhkrecipe/recipe.py
+++ models/nhkrecipe/recipe.py
@@ -4,29 +4,16 @@ import random
 from . import parser
 
 class NHKRecipe :
-   # params = {
-   #     'keyword' : '',
-   #     'timeclass' : '',
-   #     'genre[]' : '',
-   #     'calorie_min' : 0,
-   #     'calorie_max' : 0,
-   #     'year' : '',
-   #     'month' : '',
-   #     'pg' : ''
-   # }
     def __init__(self) :
         self.__root_url = 'https://www.kyounoryouri.jp'
   
-    def get_recipe_num(self):
+    def get_recipe_num(self, params):
         """
         get the number of recipes
         return
              success : correct number
              error   : -1
         """
-        params = {
-            'keyword' : ''
-        }
         req = urllib.request.Request('{}?{}'.format(self.__root_url+'/search/recipe', urllib.parse.urlencode(params)))
         with urllib.request.urlopen(req) as res :
             body = res.read().decode(encoding='utf-8')
@@ -36,17 +23,13 @@ class NHKRecipe :
             recipe_num_parser.close()
         return int(recipe_num_parser.num)
         
-    def get_recipe_list(self, page):
+    def get_recipe_list(self, params):
         """
         get recipes from page 'page'
         return 
             success : recipe list
             error   : empty list
         """
-        params = {
-            'keyword' : '',
-            'pg' : page
-        }
         req = urllib.request.Request('{}?{}'.format(self.__root_url+'/search/recipe', urllib.parse.urlencode(params)))
         with urllib.request.urlopen(req) as res :
             body = res.read().decode(encoding='utf-8')
@@ -59,24 +42,47 @@ class NHKRecipe :
 
     def get_random_recipe(self) :
         """
-        get a recipe
+        get a recipe 
+        return
+            success : recipe url
+            error : empty string
+        """
+        params = {
+            'keyword' : ''
+        }
+        return self.get_random_recipe_by_params(params)
+
+    
+    def get_random_recipe_by_params(self, params) :
+        """
+        get a recipe using params
+        params
+           params = {
+                'keyword' : '',    # recipe name or food
+                'timeclass' : '',  # ???
+                'genre[]' : '',    # 1=Japanese, 2=Western, 3=Chinese, 4=Korean, 5=ethnic, 6=etc
+                'calorie_min' : 0, # 
+                'calorie_max' : 0, # 
+                'year' : '',       # when the recipe registered
+                'month' : '',      # when the recipe registered
+            }
         return
             success : recipe url
             error : empty string
         """
         # レシピ数を取得
-        recipe_num = self.get_recipe_num()
+        recipe_num = self.get_recipe_num(params)
         if recipe_num <= 0:
             return ''
 
         # レシピ数を20で割って切り上げしページ数を割り出す
         max_page = -(-recipe_num // 20)
 
+        params["pg"] = random.randrange(1, max_page, 1)
         # ランダムなページのランダムなレシピを取得する
-        recipe_list = self.get_recipe_list(random.randrange(1, max_page, 1))
+        recipe_list = self.get_recipe_list(params)
         if len(recipe_list) <= 0:
             return ''
         
         # レシピリストからランダムに選択し出力する
         return self.__root_url + recipe_list[random.randrange(0, len(recipe_list)-1, 1)]
-        
