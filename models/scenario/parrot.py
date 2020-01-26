1d1f01a510839baabdf3550409e01c929f2af547
tamamizu <0delta0alpha@gmail.com>
2020-01-26 13:58:21 +0900

    debug code

--- models/scenario/parrot.py
+++ models/scenario/parrot.py
@@ -5,6 +5,9 @@ class Parrot :
     def response(self, params):
         """
         """
+        print(self.__class__)
+        print(params.__class__)
+        print(params.keys)
         response_message = '{}ってなに？'.format(params.message)
         return response_message
 
