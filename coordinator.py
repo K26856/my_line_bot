75f24b04f5f3d9dab138eea85167561ea8e5b27a
tamamizu <0delta0alpha@gmail.com>
2020-04-26 21:18:00 +0900

    change markov using memory to using db

--- coordinator.py
+++ coordinator.py
@@ -1,7 +1,7 @@
 from models.cooking.nhkrecipe import recipe
 from models.scenario import responder
 from models.scenario import dictionary
-from models.tools.markov import HiddenMarkovModel
+from models.tools.hmm import HiddenMarkovModel
 from random import choice, randrange
 
 class Coordinator :
@@ -12,7 +12,7 @@ class Coordinator :
         # 1 : absent
         self.__bot_user_status = 0 
         self.__dictionary = dictionary.Dictionary()
-        self.__markov = HiddenMarkovModel()
+        self.__markov = HiddenMarkovModel(chain=3)
         self.__responders = {
             'parrot' : responder.Parrot(self.__dictionary, self.__markov), 
             'random' : responder.RandomTalker(self.__dictionary, self.__markov),
